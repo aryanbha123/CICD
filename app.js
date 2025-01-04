38daae1ed09557dc6cab98ab0643ea5fc9b48072
Aryanbha123 <Aryanbhandari4077@gmail.com>
2025-01-04 19:42:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/' , (req,res) =>{
+    res.send('Hello World')
+} )
 
-app.listen('/' , (req,res) => {
-    res.send('Server is running on port 3000')
+app.listen(3000 , (req,res) => {
+    console.log('Server is running on port 3000');
 })
