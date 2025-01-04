10e56e46808abf7571a378d736de80e488e56ef7
Aryanbha123 <Aryanbhandari4077@gmail.com>
2025-01-04 20:25:09 +0530

    push

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/' , (req,res) =>{
-    res.send('CICD Working')
+    res.send('CICD Working 2')
 } )
 
 app.listen(3000 , (req,res) => {
