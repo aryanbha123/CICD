d1bd11fb593e72268ec36dedf0d520df4b83c217
Aryanbha123 <Aryanbhandari4077@gmail.com>
2025-01-04 20:27:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/' , (req,res) =>{
-    res.send('CICD Working 2')
+    res.send('CICD Working 3')
 } )
 
 app.listen(3000 , (req,res) => {
